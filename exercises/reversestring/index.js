0bd59f7a73599838f44084f42fcce7a4f66adbfc
mervyns <mervyns@gmail.com>
2018-12-07 01:32:08 +0800

    reversed string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,8 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  return str.split('').reduce((reversed, char) => char + reversed, '')
+}
 
 module.exports = reverse;
